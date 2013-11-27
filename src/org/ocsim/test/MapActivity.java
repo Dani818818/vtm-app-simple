5689113e9d0e461ecd2765e0fe6697363daf9840
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-11-27 12:13:19 +0100

    update example

--- src/org/ocsim/test/MapActivity.java
+++ src/org/ocsim/test/MapActivity.java
@@ -1,19 +1,16 @@
 package org.ocsim.test;
 
-import org.oscim.database.MapDatabases;
-import org.oscim.database.MapOptions;
-import org.oscim.layers.tile.BitmapTileLayer;
-import org.oscim.layers.tile.MapTileLayer;
-import org.oscim.layers.tile.bitmap.MapQuestAerial;
-import org.oscim.overlay.GenericOverlay;
-import org.oscim.renderer.overlays.GridOverlay;
+import org.oscim.core.MapPosition;
+import org.oscim.layers.tile.vector.MapTileLayer;
 import org.oscim.theme.InternalRenderTheme;
+import org.oscim.tilesource.TileSource;
+import org.oscim.tilesource.oscimap4.OSciMap4TileSource;
 import org.oscim.view.MapView;
 
 import android.os.Bundle;
 import android.view.Menu;
 
-public class MapActivity extends org.oscim.view.MapActivity {
+public class MapActivity extends org.oscim.android.MapActivity {
 
 	private MapView mMap;
 
@@ -23,38 +20,25 @@ public class MapActivity extends org.oscim.view.MapActivity {
 		setContentView(R.layout.activity_map);
 
 		mMap = (MapView) findViewById(R.id.mapView);
-
-		// Tile overlay
-		// MapTileLayer over = new MapTileLayer(mMap);
-		// over.setMapDatabase(new MapOptions(MapDatabases.TEST_READER));
-		// over.setRenderTheme(InternalRenderTheme.TRONRENDER);
-		// mMap.getOverlayManager().add(over);
-
-		// Base Layer
-		//MapOptions options = new MapOptions(MapDatabases.PBMAP_READER);
-		//options.put("url", "http://city.informatik.uni-bremen.de:80/osmstache/test/");
-
-
-		MapOptions options = new MapOptions(MapDatabases.OSCIMAP_READER);
-		options.put("url", "http://city.informatik.uni-bremen.de:80/osci/map-live/");
-
-		MapTileLayer l = mMap.setBaseMap(options);
-
-		// for now other TileLayer need to be added before the baseLayer
-		// as the last loaded RenderTheme provides the background color
-		//l.setRenderTheme(InternalRenderTheme.DEFAULT);
-		l.setRenderTheme(InternalRenderTheme.TRONRENDER);
-		//mMap.getOverlayManager().add(new TestTileLayer(mMap));
-
-		//mMap.setBaseMap(new BitmapTileLayer(mMap, OpenStreetMapMapnik.INSTANCE));
-
-		mMap.setBackgroundMap(new BitmapTileLayer(mMap, MapQuestAerial.INSTANCE));
-
-
-		mMap.getLayerManager().add(new GenericOverlay(mMap,new GridOverlay(mMap)));
-
 		mMap.setClickable(true);
 		mMap.setFocusable(true);
+
+		TileSource tileSource = new OSciMap4TileSource();
+		tileSource.setOption("url", "http://city.informatik.uni-bremen.de/tiles/vtm");
+
+		MapTileLayer l = mMapView.setBaseMap(tileSource);
+
+		l.setRenderTheme(InternalRenderTheme.DEFAULT);
+		//l.setRenderTheme(InternalRenderTheme.TRONRENDER);
+
+		//mMap.setBackgroundMap(new BitmapTileLayer(mMap, MapQuestAerial.INSTANCE));
+
+		//mMapView.getLayerManager().add(new GenericOverlay(mMapView, new GridRenderLayer(mMapView)));
+
+		MapPosition p = new MapPosition();
+		p.setZoomLevel(14);
+		p.setPosition(53.08, 8.83);
+		mMapView.setMapPosition(p);
 	}
 
 	@Override
